cd7560e090c108cfd573fc3275879879a4766389
Tomasz <tomasz30@schule.bremen.de>
2026-01-21 13:11:06 +0100

    Updated sidebar in Dashboard + cleanup in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,23 @@ import {UnsecuredFoo} from "./pages/UnsecuredFoo.tsx";
 import { AppLayout } from "./components/layout/AppLayout";
 import RequireAuth from "./auth/RequireAuth.tsx";
 import {EmployeeTable} from "./pages/EmployeeTable.tsx";
-import Footer from './components/Footer.tsx';
 import Dashboard from './pages/Dashboard';
-import { Layout } from './components/Layout';
 import {SecuredBar} from "./pages/SecuredBar.tsx";
-import {Home} from "./pages/Home.tsx";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Navigate to="/dashboard" replace />}/>
-            <Route path="/dashboard" element={<Layout><Dashboard/></Layout>}/>
-            <Route path="/foo" element={<Layout><UnsecuredFoo/></Layout>}/>
-            <Route path="/bar" element={
-                <Layout>
-                  <RequireAuth>
-                      <SecuredBar/>
-                  </RequireAuth>
-                </Layout>
-            }/>
-            <Route path="/employees" element={<Layout><EmployeeTable/></Layout>}/>
+            <Route element={<AppLayout />}>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/foo" element={<UnsecuredFoo/>}/>
+                <Route path="/bar" element={
+                    <RequireAuth>
+                        <SecuredBar/>
+                    </RequireAuth>
+                }/>
+                <Route path="/employees" element={<EmployeeTable/>}/>
+            </Route>
         </Routes>
     )
 }
