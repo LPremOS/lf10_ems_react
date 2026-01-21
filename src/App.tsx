954d7983debe759cc6e38f4269d267d8a1bb19ce
IbrahimQedirli <ibrahim.qedirli@zarm.uni-bremen.de>
2026-01-21 11:43:43 +0100

    app.tsx und employeetable änderungen für sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import './App.css'
 import {Route, Routes, Navigate} from "react-router-dom";
 import {UnsecuredFoo} from "./pages/UnsecuredFoo.tsx";
-import {SecuredBar} from "./pages/SecuredBar.tsx";
+import { AppLayout } from "./components/layout/AppLayout";
 import RequireAuth from "./auth/RequireAuth.tsx";
 import {EmployeeTable} from "./pages/EmployeeTable.tsx";
+import {SecuredBar} from "./pages/SecuredBar.tsx";
+import {Home} from "./pages/Home.tsx";
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Navigate to="/employees" replace />}/>
-            <Route path="/foo" element={<UnsecuredFoo/>}/>
-            <Route path="/bar" element={
-                <RequireAuth>
-                    <SecuredBar/>
-                </RequireAuth>
-            }/>
-            <Route path="/employees" element={<EmployeeTable/>}/>
+            <Route element={<AppLayout />}>
+                <Route path="/" element={<Navigate to="/employees" replace />}/>
+                <Route path="/dashboard" element={<Home />}/>
+                <Route path="/employees" element={<EmployeeTable/>}/>
+                <Route path="/qualifications" element={<div>Qualifikationen (Coming Soon)</div>}/>
+                <Route path="/foo" element={<UnsecuredFoo/>}/>
+                <Route path="/bar" element={
+                    <RequireAuth>
+                        <SecuredBar/>
+                    </RequireAuth>
+                }/>
+            </Route>
         </Routes>
     )
 }
--- src/pages/EmployeeTable.tsx
+++ src/pages/EmployeeTable.tsx
@@ -1,10 +1,7 @@
-import { Layout } from "../components/Layout";
 import { EmployeeOverview } from "../components/EmployeeOverview";
 
 export function EmployeeTable() {
     return (
-        <Layout>
             <EmployeeOverview />
-        </Layout>
     );
 }
