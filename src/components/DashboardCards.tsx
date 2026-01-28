1e2104c9aac14052e1f68a464fb624fe84257375
Tomasz <tomasz30@schule.bremen.de>
2026-01-28 10:38:07 +0100

    Dashboard Handyübersicht Anpassun, erweiterung von Karten damit sie für jede Bildschirmgroße gut aussehen

--- src/components/DashboardCards.tsx
+++ src/components/DashboardCards.tsx
@@ -1,23 +1,17 @@
 import "./DashboardCards.css";
 
-type DashboardCardsProps = {
-  employeecount: number;
-  qualificationscount: number;
-};
 
-
-const DashboardCards = ({ employeecount, qualificationscount }: DashboardCardsProps) => (
-<div className="dashboard-cards-row">
+const DashboardCards = ({ employeecount, qualificationscount }) => (
+  <div className="dashboard-cards-container">
     <div className="dashboard-card">
-        <div className="dashboard-card-value">{employeecount}</div>
-        <div className="dashboard-card-albel">Mitarbeiter</div>    
+      <div className="dashboard-card-value">{employeecount}</div>
+      <div className="dashboard-card-label">Mitarbeiter</div>
     </div>
     <div className="dashboard-card">
-        <div className="dashboard-card-value">{qualificationscount}</div>
-        <div className="dashboard-card-albel">Qualifikationen</div>    
-    </div>   
-</div>
-
+      <div className="dashboard-card-value">{qualificationscount}</div>
+      <div className="dashboard-card-label">Qualifikationen</div>
+    </div>
+  </div>
 );
 
 export default DashboardCards;
